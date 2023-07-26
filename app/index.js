468d2885aad936d5e0a189c16f3c8539441b1d97
Reza Souri <rezasouri654@gmail.com>
2023-07-26 13:06:31 +0330

    modify errors in app.js

--- app/index.js
+++ app/index.js
@@ -3,30 +3,58 @@ import bodyParser from "body-parser";
 import http from "http";
 import { error } from "console";
 const app = express();
+let users = [];
 
 function App() {
   const server = http.createServer(app);
   app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended:true }));
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-  app.get('/',(req,res)=>{
-    res.json({"name": "farshid koni"})
-  })
-  app.post('/signup',(req,res)=>{
-    var errors = []
-    var pattern = /^[a-z0-9]{1,}@[a-z0-9]{1,}\.[a-z]{1,}$/i
-    if (!pattern.test(res.body.email) || res.body.password <6){
-      if (!pattern.test(res.body.email)) {
-        errors.push({key:"email",errorText:"فرمت ایمیل وارد شده صحیح نمی باشد"})
+  app.get("/", (req, res) => {
+    res.json({ name: "farshid koni" });
+  });
+  app.post("/signup", (req, res) => {
+    try {
+      var errors = [];
+      var pattern = /^[a-z0-9]{1,}@[a-z0-9]{1,}\.[a-z]{1,}$/i;
+      if (!pattern.test(req.body.email) || req.body.password.length < 6) {
+        if (!pattern.test(req.body.email)) {
+          errors.push({
+            key: "email",
+            errorText: "فرمت ایمیل وارد شده صحیح نمی باشد",
+          });
+        }
+        if (req.body.password.length < 6) {
+          errors.push({
+            key: "password",
+            errorText: "طول پسوورد کمتر از 6 کاراکتر می باشد",
+          });
+        }
+        res.status(400).json({ errors: errors });
       }
-      if (res.body.password <6){
-        errors.push({key:"password",errorText:"طول پسوورد کمتر از 6 کاراکتر می باشد"})
+      var err = false;
+      users.map((user) => {
+        if (user.email === req.body.email) {
+          err = true;
+        }
+      });
+      if (err) {
+        errors.push([
+          { key: "email", errorText: "با این ایمیل قبلا ثبت نام کرده اید" },
+        ]);
+        res.status(400).json({ errors: errors });
       }
-      res.status(400).json({errors:errors})
-
+      users.push({
+        email: req.body.email,
+        password: req.body.password,
+        token: "",
+      });
+      console.log(users);
+      res.status(201).json(req.body);
+    } catch (e) {
+      res.status(500).json([]);
     }
-    res.status(201).json(req.body)
-  })
+  });
 
   server.listen(3000, () => {
     console.log("server running on 5000 port");
